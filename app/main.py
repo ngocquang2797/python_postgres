98493d12959c34d9944579cf0fb975a92482de1a
Quang Nguyen <ngocquang2797@gmail.com>
2020-12-17 01:04:05 +0700

    update requirements.txt

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ class dataLoader():
         self.port = "5432"
         self.user = "username"
         self.pwd = "secret"
-        self.chuck_size = 10000
+        self.chuck_size = 40000
 
     def file_header(self, file_path):
         unique_headers = set()
@@ -33,20 +33,18 @@ class dataLoader():
                 unique_headers.update(next(csvin, []))
                 return list(unique_headers)
 
-    def pg_load_table(self, file_path, table_name):
+    def pg_load_table(self, file_path, table_name, columns=None):
         try:
             engine = create_engine('postgres://{}:{}@{}:{}/{}'.format(self.user, self.pwd, self.host, self.port, self.dbname))
             count = 0
             for df in pd.read_csv(file_path, chunksize=self.chuck_size):
-                df = df.replace(r'^\s*$', np.nan, regex=True)
+                # df.astype({'ru11ind': 'object'})
                 df.to_sql(
                     table_name,
                     engine,
                     index=False,
                     if_exists='append'  # if the table already exists, append this data
                 )
-                if count == 1000000:
-                    break
                 count += self.chuck_size
                 print("{0} rows".format(count), end="\r")
             print("Success!!")
