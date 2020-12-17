19681f08edecfb5dc58512e5fda52c4586000dc0
Quang Nguyen <ngocquang2797@gmail.com>
2020-12-17 10:13:38 +0700

    update

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import csv
 from glob import iglob
 import pandas as pd
 from sqlalchemy import create_engine
+import sqlalchemy
 import numpy as np
 
 import argparse
@@ -12,6 +13,10 @@ def parse_args():
     parser = argparse.ArgumentParser("Import data from csv to postgresDB")
     parser.add_argument("-p", "--path", type=str, help="path to .csv files")
     parser.add_argument("-t", "--tablename", type=str, help="set name to table")
+    parser.add_argument("-m", "--mode", type=str, default="replace", help="append: apppend if existed \nreplace: replace")
+    parser.add_argument('-c', '--columns', action='store', dest='alist',
+                        type=str, nargs='*', default=[],
+                        help="Examples: -i fid wd19nm, -i lad19cd")
     args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
     return parser.parse_args()
@@ -23,7 +28,7 @@ class dataLoader():
         self.port = "5432"
         self.user = "username"
         self.pwd = "secret"
-        self.chuck_size = 40000
+        self.chuck_size = 10000
 
     def file_header(self, file_path):
         unique_headers = set()
@@ -33,17 +38,29 @@ class dataLoader():
                 unique_headers.update(next(csvin, []))
                 return list(unique_headers)
 
-    def pg_load_table(self, file_path, table_name, columns=None):
+    def gettype(self, cols):
+        types = {}
+        for col in cols:
+            types[col] = sqlalchemy.types.String()
+        return types
+
+    def pg_load_table(self, file_path, table_name, m='replace', colums = None):
+        dtypes = self.gettype(colums)
         try:
             engine = create_engine('postgres://{}:{}@{}:{}/{}'.format(self.user, self.pwd, self.host, self.port, self.dbname))
             count = 0
+            # read csv chuck by chuck
             for df in pd.read_csv(file_path, chunksize=self.chuck_size):
-                # df.astype({'ru11ind': 'object'})
+                # replace space value to nan
+                df = df.replace(r'^\s*$', np.nan, regex=True)
+                df = df.replace('', np.nan, regex=True)
+                # import data to sql
                 df.to_sql(
                     table_name,
                     engine,
                     index=False,
-                    if_exists='append'  # if the table already exists, append this data
+                    dtype=dtypes,
+                    if_exists=m  # if the table already exists, append this data
                 )
                 count += self.chuck_size
                 print("{0} rows".format(count), end="\r")
@@ -59,7 +76,7 @@ def main():
 
     else:
         cv = dataLoader()
-        cv.pg_load_table(args.path, args.tablename)
+        cv.pg_load_table(args.path, args.tablename, args.mode, args.alist)
 
 if __name__ == "__main__":
     main()
