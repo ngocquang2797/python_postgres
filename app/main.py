a3f498bb8682319c806d0697ed7342eb84d8cdd1
Quang Nguyen <ngocquang2797@gmail.com>
2020-12-17 14:34:48 +0700

    update main.py

--- app/main.py
+++ app/main.py
@@ -9,6 +9,8 @@ import argparse
 import sys
 import time
 
+from pgsql.core import dataLoader
+
 def parse_args():
     parser = argparse.ArgumentParser("Import data from csv to postgresDB")
     parser.add_argument("-p", "--path", type=str, help="path to .csv files")
@@ -20,54 +22,6 @@ def parse_args():
     args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
     return parser.parse_args()
-
-class dataLoader():
-    def __init__(self):
-        self.dbname = "database"
-        self.host = "db"
-        self.port = "5432"
-        self.user = "username"
-        self.pwd = "secret"
-        self.chuck_size = 10000
-
-    def file_header(self, file_path):
-        unique_headers = set()
-        for filename in iglob(file_path):
-            with open(filename, 'r') as fin:
-                csvin = csv.reader(fin)
-                unique_headers.update(next(csvin, []))
-                return list(unique_headers)
-
-    def gettype(self, cols):
-        types = {}
-        for col in cols:
-            types[col] = sqlalchemy.types.String()
-        return types
-
-    def pg_load_table(self, file_path, table_name, m='replace', colums = None):
-        dtypes = self.gettype(colums)
-        try:
-            engine = create_engine('postgres://{}:{}@{}:{}/{}'.format(self.user, self.pwd, self.host, self.port, self.dbname))
-            count = 0
-            # read csv chuck by chuck
-            for df in pd.read_csv(file_path, chunksize=self.chuck_size):
-                # replace space value to nan
-                df = df.replace(r'^\s*$', np.nan, regex=True)
-                df = df.replace('', np.nan, regex=True)
-                # import data to sql
-                df.to_sql(
-                    table_name,
-                    engine,
-                    index=False,
-                    dtype=dtypes,
-                    if_exists=m  # if the table already exists, append this data
-                )
-                count += self.chuck_size
-                print("{0} rows".format(count), end="\r")
-            print("Success!!")
-
-        except Exception as e:
-            print("Error: {}".format(str(e)))
 
 def main():
     args = parse_args()
