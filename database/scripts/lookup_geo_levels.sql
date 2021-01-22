139e3321370708b843331d410d01d19ada44bac7
Quang Nguyen <ngocquang2797@gmail.com>
2021-01-22 10:22:46 +0700

    update sql scripts

--- database/scripts/lookup_geo_levels.sql
+++ database/scripts/lookup_geo_levels.sql
@@ -1,27 +1,58 @@
+-- "ONSPD_MAY_2020_UK"
+drop table if exists onspd_uk CASCADE;
+create table onspd_uk as
+    (
+        select pcd, oslaua, pcon
+        from "ONSPD_MAY_2020_UK"
+    );
+
+drop table if exists pcd_msoa CASCADE;
+create table pcd_msoa as
+    (
+        -- combine data from pcd_wd ,"ONSPD_MAY_2020_UK" and msoa_lsoa to get pcd7, lau1, lau2
+        select coalesce(pcd_wd.pcd7, msoa_lsoa.pcd7) as pcd7,
+                   coalesce(pcd_wd.lad11cd, msoa_lsoa.ladcd) as lau1,
+                   pcd_wd.wd11cd as lau2
+            from msoa_lsoa
+            full outer join pcd_wd on msoa_lsoa.pcd7 = pcd_wd.pcd7
+    );
+
+drop table if exists pcd_merge CASCADE;
+create table pcd_merge as
+    (
+        select coalesce(pcd_msoa.pcd7, onspd_uk.pcd) as pcd7,
+               coalesce(pcd_msoa.lau1, onspd_uk.oslaua) as lau1,
+               pcd_msoa.lau2 as lau2,
+               onspd_uk.pcon as pc
+
+        from pcd_msoa
+        full outer join onspd_uk on pcd_msoa.pcd7 = onspd_uk.pcd
+);
+
+-- drop table
+drop table onspd_uk;
+drop table pcd_msoa;
+
+-- create backup Q1
+drop table if exists lookup_geo_levels_q1 CASCADE;
+create table lookup_geo_levels_q1 as
+    (
+        -- combine data from pcd_wd ,"ONSPD_MAY_2020_UK" and msoa_lsoa to get pcd7, lau1, lau2
+    -- get pcd from pcd7
+            select RTRIM(LEFT(pcd_merge.pcd7,4),' ')  as pcd,
+               pcd_merge.lau1, pcd_merge.lau2, pcd_merge.pc
+            from pcd_merge
+            group by pcd, lau1, lau2, pc
+    );
+
+drop table pcd_merge
+--
 -- create lookup_geo_levels_code_table
 drop table if exists lookup_geo_levels_code CASCADE;
 create table lookup_geo_levels_code
 as
 (
-    -- combine data from pcd_wd and msoa_lsoa to get pcd7, lau1, lau2
-    with Q1 as
-        (
-            select coalesce(pcd_wd_new.pcd7, msoa_lsoa_new.pcd7, "ONSPD_MAY_2020_UK".pcd) as pcd7,
-                   coalesce(pcd_wd_new.lad11cd, msoa_lsoa_new.ladcd, "ONSPD_MAY_2020_UK".oslaua) as lau1,
-                   pcd_wd_new.wd11cd as lau2,
-                   "ONSPD_MAY_2020_UK".pcon as pc
-            from msoa_lsoa_new
-            full outer join pcd_wd_new on msoa_lsoa_new.pcd7 = pcd_wd_new.pcd7
-            full outer join "ONSPD_MAY_2020_UK" on pcd_wd_new.pcd7 = "ONSPD_MAY_2020_UK".pcd
-        ),
-    -- get pcd from pcd7
-    Q2 as
-        (
-            select RTRIM(LEFT(Q1.pcd7,4),' ')  as pcd,
-               Q1.lau1, Q1.lau2, Q1.pc
-            from Q1
-            group by pcd, lau1, lau2, pc
-        ),
+    with
     --      combine data from local_authority and geo_level to get lau1, lau2, nuts1, nuts2, nuts3
     Q3 as
         (
@@ -39,17 +70,17 @@ as
     Q4 as
         (
             select
-            Q2.pcd,
-            Q2.pc,
+            lookup_geo_levels_q1.pcd,
+            lookup_geo_levels_q1.pc,
             -- 	Standardize data by choosing a value
-            coalesce(Q3.lau1, Q2.lau1) as lau1,
-            coalesce(Q3.lau2, Q2.lau2) as lau2,
+            coalesce(Q3.lau1, lookup_geo_levels_q1.lau1) as lau1,
+            coalesce(Q3.lau2, lookup_geo_levels_q1.lau2) as lau2,
             Q3.nuts3,
             Q3.nuts2,
             Q3.nuts1
-                    from Q2
+                    from lookup_geo_levels_q1
             -- combine data
-            full outer join Q3 on Q2.lau2 = Q3.lau2 and Q2.lau1 = Q3.lau1
+            full outer join Q3 on lookup_geo_levels_q1.lau2 = Q3.lau2 and lookup_geo_levels_q1.lau1 = Q3.lau1
         ),
     --      combine data from Q3 and Q4 to get pc column
     Q5 as
@@ -63,7 +94,7 @@ as
                    Q4.nuts3,
                    coalesce(Q4.pc,lau2_pc_la.pcon19cd) as pc
             from Q4
-            full outer join lau2_pc_la on Q4.lau2 = lau2_pc_la.wd19cd and Q4.lau1 = lau2_pc_la.lad19cd
+            full outer join lau2_pc_la on Q4.lau2 = lau2_pc_la.wd19cd and Q4.lau1 = lau2_pc_la.lad19cd and Q4.pc = lau2_pc_la.pcon19cd
         )
 
 
@@ -71,6 +102,8 @@ as
     from Q5
     group by pcd, lau1, lau2, nuts1, nuts2, nuts3, pc
 );
+
+drop table lookup_geo_levels_q1;
 
 -- create lookup_geo_levels_id
 drop table if exists lookup_geo_levels_id CASCADE;
@@ -94,8 +127,7 @@ as
     left join lookup_pc on lookup_geo_levels_code.pc = lookup_pc.code
 );
 
-alter table lookup_geo_levels_id add column id serial,
-    add foreign key (lau1_id) references lookup_lau1(id),
+alter table lookup_geo_levels_id add foreign key (lau1_id) references lookup_lau1(id),
     add foreign key (lau2_id) references lookup_lau2(id),
     add foreign key (nuts1_id) references lookup_nuts1(id),
     add foreign key (nuts2_id) references lookup_nuts2(id),
